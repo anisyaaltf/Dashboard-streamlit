8700b6175a89799d4972714f08ff204a521f42f1
anisyaaltf <109120261+anisyaaltf@users.noreply.github.com>
2026-04-05 00:50:16 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,87 +4,86 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime as dt
 
-    #config
-    st.set_page_config(page_title="Dashboard penjualan", layout="wide")
-    st.title("Dashboard analisis penjualan")
+#config
+st.set_page_config(page_title="Dashboard penjualan", layout="wide")
+st.title("Dashboard analisis penjualan")
 
     # load data
 
-    @st.cache_data
-    def load_data():
-        df = pd.read_csv('main_data')
-    df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])        return df
+@st.cache_data
+def load_data():
+    df = pd.read_csv('main_data.csv')
+    df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])        
     df['month'] = df['order_purchase_timestamp'].dt.to_period('M').astype(str)
     return df
 
-    df = load_data()
+df = load_data()
 
-    #sidebar filter
-    st.sidebar.header("filter")
-    month_list = sorted(df['month'].unique())
-    selected_month = st.sidebar.multiselect(
+#sidebar filter
+st.sidebar.header("filter")
+month_list = sorted(df['month'].unique())
+selected_month = st.sidebar.multiselect(
         "pilih bulan", 
-        option=month_list,
+        options=month_list,
         default=month_list
-    )
-    df_filtered = df[df['month'].isin(selected_month)]
+)
+df_filtered = df[df['month'].isin(selected_month)]
 
-    #KPI
-    total_revenue = df_filtered['price'].sum()
-    total_orders = df_filtered['order_id'].unique()
-    total_customers = df_filtered['customer_id'].unique()
+#KPI
+total_revenue = df_filtered['price'].sum()
+total_orders = df_filtered['order_id'].nunique()
+total_customers = df_filtered['customer_id'].nunique()
 
-    col1,col2 = st.columns(2)
-    with col1:
-    col1.metric("Total Revenue", f"{total_revenue:, 0f}")
-    col2.metric("Total Orders", total_orders)
-    col3.metric("Total Customer", total_customers)
+col1, col2 , col3 = st.columns(3)
 
-    # Pertanyaan 1
-    st.subheader("Performa penjualan & Revenue")
-    monthly_sales = df_filtered.groupby('month').agg({
-        'price':'sum'
-        'order_id':'count'
-    }).reset_index()
-    col1, col2 = st.columns(2)
-    with col1:
-        st.markdown("Revenue Bulanan")
-        st.line_chart(monthly_sales.set_index('month')['price'])
-    with col2:
-        st.markdown("Jumlah order")
-        st.line_chart(monthly_sales.set_index('month')['order'])
+col1.metric("Total Revenue", f"{total_revenue:,.0f}")
+col2.metric("Total Orders", total_orders)
+col3.metric("Total Customer", total_customers)
 
-    # Pertanyaan 2
-    st.subheader("Frekuensi Pembelian Customer")
-    customer_freq = df_filtered.groupby('customer_id')['order_id'].count().reset_index()
-    customer_freq.columns = ['customer_id', 'frequency']
+# Pertanyaan 1
+st.subheader("Performa penjualan & Revenue")
+monthly_sales = df.groupby('month').agg({
+    'price':'sum',
+    'order_id': 'count'
+}).reset_index()
+col1, col2 = st.columns(2)
+with col1:
+    st.markdown("Revenue Bulanan")
+    st.line_chart(monthly_sales.set_index('month')['price'])
+with col2:
+    st.markdown("Jumlah order")
+    st.line_chart(monthly_sales.set_index('month')['order_id'])
 
-    fig, ax = plt.subplots()
-    sns.histplot(customer_freq['frequency'], bins=20, ax=ax)
-    ax.set_title("Distribusi Frekuensi Customer")
-    ax.set_xlabel("Jumlah Transaksi")
-    ax.set_ylabel("Jumlah Customer")
-    st.pyplot(fig)
+# Pertanyaan 2
+st.subheader("Frekuensi Pembelian Customer")
+customer_freq = df_filtered.groupby('customer_id')['order_id'].count().reset_index()
+customer_freq.columns = ['customer_id', 'frequency']
 
-    #RFM ANALYSIS
-    st.subheader("RFM ANALYSIS")
-    snapshot_date = df_filtered['order_purchase_timestamp']
-    rfm = df_filtered.groupby('customer_id').agg({
-        'order_purchase_timestamp':lambda x:(snapshot_date - x.max()).days, 
-        'order_id':'count'
-        'price':'sum'
-    })
-    rfm.columns = ['Regency', 'Frequency', 'Monetary']
-    rfm.reset_index(inplace=True)
+fig, ax = plt.subplots()
+sns.histplot(customer_freq['frequency'], bins=20, ax=ax)
+ax.set_title("Distribusi Frekuensi Customer")
+ax.set_xlabel("Jumlah Transaksi")
+ax.set_ylabel("Jumlah Customer")
+st.pyplot(fig)
 
-    # tampil top customer
-    st.markdown("Top Customers(RFM)")
-    st.dataframe(rfm.sort_values(by='Monetary', ascending=False).head())
+#RFM ANALYSIS
+st.subheader("RFM ANALYSIS")
+snapshot_date = df_filtered['order_purchase_timestamp'].max()
+rfm = df_filtered.groupby('customer_id').agg({
+    'order_purchase_timestamp':lambda x:(snapshot_date - x.max()).days, 
+    'order_id':'count',
+    'price':'sum'
+ })
+rfm.columns = ['Regency', 'Frequency', 'Monetary']
+rfm = rfm.reset_index()
+# tampil top customer
+st.markdown("Top Customers(RFM)")
+st.dataframe(rfm.sort_values(by='Monetary', ascending=False).head())
 
-    # Top state
-    st.subheader("Top Customer State")
-    top_state = df_filtered['customer_state'].value_counts().head(10)
-    st.bar_chart(top_state)
+# Top state
+st.subheader("Top Customer State")
+top_state = df_filtered['customer_state'].value_counts().head(10)
+st.bar_chart(top_state)
 
-    #footer
-    st.caption(Dashboard by Anisya Lutfiyani)
+#footer
+st.caption("Dashboard by Anisya Lutfiyani")
