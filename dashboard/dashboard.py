0c8938593b33b3645cae45ae4adc20e9ad67a67d
anisyaaltf <109120261+anisyaaltf@users.noreply.github.com>
2026-04-05 00:52:43 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -12,7 +12,7 @@ st.title("Dashboard analisis penjualan")
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv('main_data.csv')
+    df = pd.read_csv('data/main_data.csv')
     df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])        
     df['month'] = df['order_purchase_timestamp'].dt.to_period('M').astype(str)
     return df
